838b6cba99c0a255ea882560dddab999da0256a5
eddolody-ui <eddolody@gmail.com>
2026-02-11 10:45:05 +0630

    fix env

--- server/dist/config/envVars.js
+++ server/dist/config/envVars.js
@@ -4,7 +4,7 @@ exports.ENV_VARS = void 0;
 require("dotenv/config");
 exports.ENV_VARS = {
     PORT: process.env.PORT || 5000,
-    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
+    CLIENT_URL: process.env.CLIENT_URL || "https://retailshop-duv7.onrender.com",
     MONGO_URI: process.env.MONGO_URI,
     NODE_ENV: process.env.NODE_ENV,
     ACCESS_JWT_SECRET: process.env.ACCESS_JWT_SECRET,
--- server/src/config/envVars.ts
+++ server/src/config/envVars.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 export const ENV_VARS = {
   PORT: process.env.PORT || 5000,
-  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
+  CLIENT_URL: process.env.CLIENT_URL || "https://retailshop-duv7.onrender.com",
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
   ACCESS_JWT_SECRET: process.env.ACCESS_JWT_SECRET,
